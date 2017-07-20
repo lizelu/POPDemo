c42c60fb3efa261af045faaa6b994ab6f6b104b3
zeluliCredit <zeluli@creditease.cn>
2017-07-20 10:03:35 +0800

    update source code

--- PopDemo/ViewController.swift
+++ PopDemo/ViewController.swift
@@ -10,19 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var subLayer: CALayer!
+    var repeatTag = true
+    
     @IBOutlet var testView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.subLayer = CALayer()
+        self.view.layer.addSublayer(self.subLayer)
+        resetSublayer()
+        addSpringAnimation(type: kPOPLayerTranslationX)
+    }
+    
+    func resetSublayer() {
+        self.subLayer.pop_removeAllAnimations()
+        self.subLayer.opacity = 1.0
+        self.subLayer.transform = CATransform3DIdentity
+        self.subLayer.masksToBounds = true
+        self.subLayer.backgroundColor = UIColor.orange.cgColor
+        self.subLayer.bounds = CGRect(origin: CGPoint.zero, size: CGSize(width: 80, height: 80))
+        self.subLayer.cornerRadius = self.subLayer.bounds.width / 2
+        self.subLayer.position = CGPoint(x: self.view.center.x, y: 200)
     }
 
-    @IBAction func tapButton(_ sender: Any) {
-        if let anim = POPSpringAnimation(propertyNamed: kPOPLayerBounds) {
-            anim.toValue = NSValue(cgRect: CGRect(x: 10, y: 10, width: 40, height: 40))
-            testView.pop_add(anim, forKey: "size")
+    func addSpringAnimation(type: String) {
+        self.subLayer.pop_removeAllAnimations()
+        
+        let springAnimation = POPSpringAnimation(propertyNamed: type)
+        springAnimation?.springBounciness = 10
+        
+        if type == kPOPLayerTranslationX {
+            if repeatTag {
+                springAnimation?.toValue = 100
+            } else {
+                springAnimation?.toValue = -100
+            }
         }
         
+        self.subLayer.pop_add(springAnimation, forKey: <#T##String!#>)
+        
     }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
